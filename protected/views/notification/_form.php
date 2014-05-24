2fa7f499be4218f93fc93668eed39be3087c7e23
Jakub Wawrzyniak <jakub@itpeople.pl>
2014-05-24 23:28:47 +0200

    tpl fix

--- protected/views/notification/_form.php
+++ protected/views/notification/_form.php
@@ -10,7 +10,7 @@
 )); ?>
 
 <?php echo $form->errorSummary($model); ?>
-<?php echo $form->dropDownListRow($model, 'group_id', $groups); ?>
+<?php echo $form->dropDownListRow($model, 'group_id', $groups, array('placeholder'=>'')); ?>
 <?php echo $form->textFieldRow($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
 <?php echo $form->markdownEditorRow($model, 'body', array('height'=>'170px')); ?>
 <?php echo $form->fileFieldRow($model, 'attachment'); ?>
