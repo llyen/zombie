c47ec4014295bcb77c33fe65c3064aa401ac3ae7
Jakub Wawrzyniak <jakub@itpeople.pl>
2014-05-25 18:17:00 +0200

    preview

--- protected/views/resource/_view_items.php
+++ protected/views/resource/_view_items.php
@@ -98,3 +98,85 @@ $this->widget('bootstrap.widgets.TbLabel', array(
     </table>
 </div>
 <?php endif; ?>
+<?php
+    $this->widget(
+		'bootstrap.widgets.TbLabel',
+		array(
+			'type' => 'important',
+		    'label' => 'Wieże i pułapki',
+			'htmlOptions' => array(
+				'style' => 'margin: 10px 0px; width: 100%; text-align:center; font-weight: normal; padding: 8px 0px; font-size: 16px;',	
+			),
+		)
+	);
+?>
+<?php if(!is_null($items['towers'])): ?>
+<div class="grid-view">
+    <table class="items table table-striped table-condensed">
+        <thead>
+            <tr>
+                <th></th>
+                <th style="vertical-align: middle;">Nazwa</th>
+                <th style="vertical-align: middle;">Opis</th>
+                <th style="vertical-align: middle;">Efekt</th>
+                <th style="vertical-align: middle;">Obrażenia</th>
+                <th style="vertical-align: middle;">Wzór</th>
+                <th style="vertical-align: middle;">Cena</th>
+                <th style="vertical-align: middle;">Operacje</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($items['towers'] as $tower): ?>
+                <tr>
+                    <td style="vertical-align: middle;"><?php echo '<img data-toggle="tooltip" data-placement="right" data-html="true" title="<img src=&quot;'.Yii::app()->baseUrl.'/images/objects/'.trim($tower['params']['img']).'_128x128px.png&quot; /><br /><span style=&quot;color: #ffffff;&quot;>Posiadanych: '.$tower['count'].'</span>" class="preview" src="'.Yii::app()->baseUrl.'/images/objects/'.trim($tower['params']['img']).'_64x64px.png" alt="'.trim($tower['params']['img']).'">'; ?></td>
+                    <td style="vertical-align: middle;"><?php echo $tower['params']['name']; ?></td>
+                    <td style="vertical-align: middle;">
+                        <?php echo $tower['params']['desc']; ?> 
+                    </td>
+                    <td style="vertical-align: middle;"><?php echo $tower['params']['effect']; ?></td>
+                    <td style="color: #AC1D23; vertical-align: middle;"><?php echo $tower['params']['dmg']; ?></td>
+                    <td style="vertical-align: middle;"><img src="<?php echo Yii::app()->baseUrl.'/images/preview.png'; ?>" data-toggle="tooltip" data-placement="left" data-html="true" title="<?php
+                        foreach($tower['params']['dmgPattern'] as $dmgPattern)
+                        {
+                            foreach($dmgPattern as $dmg)
+                            {
+                                echo '<img src=&quot;'.Yii::app()->baseUrl.'/images/'.$dmg.'.jpg&quot; />';
+                            }
+                            echo '<br />';
+                        }
+                        ?>" /></td>
+                    <td style="vertical-align: middle;"><?php echo $tower['params']['price']; ?></td>
+                    <td style="vertical-align: middle;">
+                        <?php
+                            $this->widget(
+                            	'bootstrap.widgets.TbButton',
+                                array(
+                                    'label' => 'Kup',
+                                    'type' => 'danger',
+                                    'size' => 'small',
+                                    'url' => array('resource/buy', 'id'=>$tower['params']['id']),
+                                    'htmlOptions' => array(
+                                        'style'=>'color: #ffffff;',
+                                    ),
+                                )
+                            );
+                        ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?php endif; ?>
+<?php
+    $this->widget(
+		'bootstrap.widgets.TbLabel',
+		array(
+			'type' => 'important',
+		    'label' => 'Broń dostępna do sprzedania',
+			'htmlOptions' => array(
+				'style' => 'margin: 10px 0px; width: 100%; text-align:center; font-weight: normal; padding: 8px 0px; font-size: 16px;',	
+			),
+		)
+	);
+?>
