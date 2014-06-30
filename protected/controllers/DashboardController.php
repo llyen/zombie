9c0635235f0b7a6c5f200f595b9d633b08dcc1c0
Jakub Wawrzyniak <jakub@itpeople.pl>
2014-06-30 23:32:12 +0200

    fix

--- protected/controllers/DashboardController.php
+++ protected/controllers/DashboardController.php
@@ -36,7 +36,8 @@ class DashboardController extends Controller
         $challenges = $player->user->group->challenges;
         $challengesCompleted = Solution::model()->findAll('player_id = :player_id and completed = 1', array(':player_id' => $player->id));
         $class = Class_::model()->earliest($player->user->group->id)->find();
-        $dashboard['leaderboardPosition'] = (int) Yii::app()->db->createCommand('select t.position from (select p.id, @rownum := @rownum + 1 as position from players p, (select @rownum := 0) r order by p.first_currency + p.second_currency desc) as t where t.id = '.$player->id)->queryRow();
+        $leaderboardPosition = Yii::app()->db->createCommand('select t.position from (select p.id, @rownum := @rownum + 1 as position from players p, (select @rownum := 0) r order by p.first_currency + p.second_currency desc) as t where t.id = '.$player->id)->queryRow();
+        $dashboard['leaderboardPosition'] = (int) $leaderboardPosition['position'];
         
         if(!is_null($badges))
         {
