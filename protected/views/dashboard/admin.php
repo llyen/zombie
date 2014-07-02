22fb5b161caeeb0b1591c6c670f467c4265675dd
Jakub Wawrzyniak <jakub@itpeople.pl>
2014-07-02 21:50:38 +0200

    dashboard name fix

--- protected/views/dashboard/admin.php
+++ protected/views/dashboard/admin.php
@@ -4,9 +4,9 @@ $this->widget(
 	'bootstrap.widgets.TbBreadcrumbs',
 	array(
 		'homeLink'=>CHtml::link('Strona główna', Yii::app()->baseUrl),
-		'links' => array('Dashboard'),
+		'links' => array('Stan gry'),
 	)
 );
 ?>
-<h2>Dashboard</h2>
+<h2>Stan gry</h2>
 <?php $this->renderPartial('_grid_admin', array('dashboard'=>$dashboard)); ?>
